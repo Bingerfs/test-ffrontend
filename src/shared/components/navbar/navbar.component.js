5f5279b52ed03400b1c59937cbe2bdb0726b6770
Julia Valentina Gutiérrez Munzón <juli_vale_1210gm@hotmail.com>
2020-07-20 21:29:37 -0400

    Add styles

--- src/shared/components/navbar/navbar.component.js
+++ src/shared/components/navbar/navbar.component.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from 'react';
 import { fade, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -13,164 +13,62 @@ import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import Avatar from "@material-ui/core/Avatar";
+import {
+    Grid
+  } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch"
-    }
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
+class Navbar extends Component {
+    render() {
+      return (
+        
+        <AppBar style={{
+            marginTop:"20px"
+        }} >
+           <Toolbar >
+             
+            <Avatar
+              style={{
+                position: "absolute",
+              
+                width: "100px",
+                height: "100px"
+              }}
+              src="https://s3.amazonaws.com/assets.mockflow.com/app/wireframepro/company/C270b08139cdd83c1e9eaab009b230cf9/projects/Mb1161875685a72a3bdbeaaa3b25a1ce41558579849255/images/thumbnails/M69dddebf849aa2e74927eff2f48d3c971584242974440"
+            />
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+>
+            <MenuItem>
+              <p>INICIO</p>
+            </MenuItem>
+            <MenuItem>
+              <p>VOLUNTARIO</p>
+            </MenuItem>
+            <MenuItem>
+              <p>ACTIVIDADES</p>
+            </MenuItem>
+            </Grid>
+            <Grid
+            container
+            direction="row"
+            justify="flex-end"
+            alignItems="center">
+            <MenuItem>
+              <p>CERRAR SESIÓN</p>
+            </MenuItem>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+       
+      );
     }
   }
-}));
+  
+  export default Navbar;
+   
 
-export default function PrimarySearchAppBar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const handleProfileMenuOpen = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = event => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <p>INICIO</p>
-      </MenuItem>
-      <MenuItem>
-        <p>VOLUNTARIO</p>
-      </MenuItem>
-      <MenuItem>
-        <p>ACTIVIDADES</p>
-      </MenuItem>
-      <MenuItem>
-        <p>CERRAR SESIÓN</p>
-      </MenuItem>
-    </Menu>
-  );
-
-  return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <Avatar
-            style={{
-              width: "70px",
-              height: "70px"
-            }}
-            src="https://s3.amazonaws.com/assets.mockflow.com/app/wireframepro/company/C270b08139cdd83c1e9eaab009b230cf9/projects/Mb1161875685a72a3bdbeaaa3b25a1ce41558579849255/images/thumbnails/M69dddebf849aa2e74927eff2f48d3c971584242974440"
-          />
-          <MenuItem>
-            <p>INICIO</p>
-          </MenuItem>
-          <MenuItem>
-            <p>VOLUNTARIO</p>
-          </MenuItem>
-          <MenuItem>
-            <p>ACTIVIDADES</p>
-          </MenuItem>
-          <MenuItem>
-            <p>CERRAR SESIÓN</p>
-          </MenuItem>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-  );
-}
+ 
+  
