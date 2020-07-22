39b418773fbe86c6b3b7dae587a9a5548d53f679
Hector Hidalgo <marce.753@gmail.com>
2020-07-22 19:23:03 -0400

    Renamed files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ScholarList from './components/ScholarListComponent'
+import VolunteerList from './components/VolunteerListComponent'
 import EventList from './components/EventListComponent'
 
 function App() {
